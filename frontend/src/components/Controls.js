8fe7864dd6fde2bb47137548f19de1ff1dab2ce4
Isheros <drilisheros@gmail.com>
2020-06-30 15:43:08 -0500

    api url cambiada

--- frontend/src/components/Controls.js
+++ frontend/src/components/Controls.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const api = process.env.API_URL
+const api = process.env.API_URL;
 
 export const Controls = () => {
 
@@ -8,7 +8,7 @@ export const Controls = () => {
         e.preventDefault();
         const sec = document.getElementById('secuencia').value;
         const ope = document.getElementById('operacion').value;
-        const res = await fetch('${api}/api/calculate', {
+        const res = await fetch(`${api}/api/calculate`, {
             method: 'POST',
             headers:{
                 'Content-Type': 'application/json'
